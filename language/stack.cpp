d582e80ece7529202b1eaa1938cf8561f8350c18
Justin Hu <justinjnhu@gmail.com>
2018-06-25 00:51:06 -0700

    Added documentation and refactored out reused code

--- language/stack.cpp
+++ language/stack.cpp
@@ -4,24 +4,20 @@
 using namespace StackLang;
 using namespace Exceptions;
 
-Stack::Stack () : head (nullptr), dataSize (0), limited (false)
+Stack::Stack () : head (nullptr), dataSize (0), limit (__LONG_MAX__)
 {}
 
-Stack::Stack (unsigned long lim) : head (nullptr), dataSize (0), limit (lim), limited (true)
+Stack::Stack (unsigned long lim) : head (nullptr), dataSize (0), limit (lim)
 {}
 
 Stack::~Stack ()
 {
-    while (head != nullptr)
-    {
-        delete head -> elm;
-        pop ();
-    }
+    clear ();
 }
 
 void Stack::push (StackElement* ptr)
 {
-    if (limited && dataSize > limit)
+    if (dataSize >= limit)
     {
         throw StackOverflowError (limit);
     }
@@ -59,6 +55,21 @@ unsigned long Stack::getLimit () const
     return limit;
 }
 
+void Stack::setLimit (unsigned long newLimit)
+{
+    if (dataSize > newLimit)
+    {
+        throw StackOverflowError (newLimit);
+    }
+
+    limit = newLimit;
+}
+
+bool Stack::empty () const
+{
+    return dataSize == 0;
+}
+
 Stack::StackIterator Stack::begin () const
 {
     return StackIterator (head);
@@ -67,4 +78,16 @@ Stack::StackIterator Stack::begin () const
 Stack::StackIterator Stack::end () const
 {
     return StackIterator (nullptr);
+}
+
+void Stack::clear ()
+{
+
+    while (head != nullptr)
+    {
+        delete head -> elm;
+        pop ();
+    }
+
+    dataSize = 0;
 }
--- language/stack.h
+++ language/stack.h
@@ -13,23 +13,48 @@ namespace StackLang
 
         class StackIterator;
 
+        /**
+         * Creates a stack, optionally with a limit on the number of elements.
+         */
         Stack ();
         Stack (unsigned long);
-        Stack (const Stack& other);
 
         ~Stack ();
-
+        
+        /**
+         * Manipulates stack elements.
+         * Pop does not delete the element, but the stack destructor does assume that when the stack is destroyed, it's managed elements should be deallocated.
+         */
         void push (StackElement* ptr);
         void pop ();
         StackElement* top ();
 
+        /**
+         * Stack iterator functions
+         */
         StackIterator begin () const;
         StackIterator end () const;
 
+        /**
+         * Gets the stack's size, size limit
+         */
         unsigned long size () const;
         unsigned long getLimit () const;
 
+        /**
+         * Sets a new limit. Throws StackOverflowError if too big.
+         */
+        void setLimit (unsigned long);
+
+        /**
+         * Returns true if the stack is empty
+         */
+        bool empty () const;
+
         private:
+
+        Stack (const Stack&);
+        Stack& operator= (const Stack&);
 
         struct Node
         {
@@ -40,7 +65,8 @@ namespace StackLang
         Node* head;
         unsigned long dataSize;
         unsigned long limit;
-        bool limited;
+
+        void clear ();
     };
 }
 
