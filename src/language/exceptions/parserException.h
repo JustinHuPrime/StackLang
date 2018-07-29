9b4c1be529877928f291345a7c4a37fa1818e20f
Justin Hu <justinjnhu@gmail.com>
2018-07-28 17:06:04 -0700

    license boilerplate formatting fixed

--- src/language/exceptions/parserException.h
+++ src/language/exceptions/parserException.h
@@ -2,7 +2,7 @@
 //
 // This file is part of the StackLang interpreter.
 //
-// The StackLang interpreter is free software: you can redistribute it and / or
+// The StackLang interpreter is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
