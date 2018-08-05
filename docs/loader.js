f2caca0a997c30688d901176982cb67234cc004e
Justin Hu <justinjnhu@gmail.com>
2018-08-04 23:12:54 -0700

    moved gcse out into loader.js, added license stuff

--- docs/loader.js
+++ docs/loader.js
@@ -1,5 +1,24 @@
 "use strict";
+
+/**
+ * This file is part of the StackLang documentation.
+ * StackLang Documentation by Justin Hu is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
 $(function () {
     $("#sidebar").load("https://justinhuprime.github.io/StackLang/sidebar.html");
     $("#footer").load("https://justinhuprime.github.io/StackLang/footer.html");
 });
+
+/**
+ * The function below is written and copyrighted by Google.
+ */
+(function () {
+    var cx = '013187363385035568625:b83vmcztnxu';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
