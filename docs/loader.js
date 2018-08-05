ef4fa9f880664883485b5de355a01727ba600032
Justin Hu <justinjnhu@gmail.com>
2018-08-04 23:07:15 -0700

    lots of documentation work done

---
+++ docs/loader.js
@@ -0,0 +1,5 @@
+"use strict";
+$(function () {
+    $("#sidebar").load("https://justinhuprime.github.io/StackLang/sidebar.html");
+    $("#footer").load("https://justinhuprime.github.io/StackLang/footer.html");
+});
